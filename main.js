db743a899ab05894136356ea5f086701efb3edaa
paulo gabriel <145785318+paulobolt@users.noreply.github.com>
2025-11-25 11:36:20 -0300

    hm

--- main.js
+++ main.js
@@ -1,125 +1,61 @@
+import { aleatorio } from "./aleatorio.js";
+import { perguntas } from "./perguntas.js";
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
-const perguntas = [
-    {
-        enunciado: "Quando foi desenvolvido a primeira copa do mundo?",
-        alternativas: [
-            {
-                texto: " 13 de julho de 1930, no Uruguai.",
-                afirmacao: [
-                " Criada em 1930 no Uruguai. ",
-                "afirmacao 2",
-                "afirmacao 3"
-
-                ]
-            },
-            {
-                texto: " Criada  13 de julho de 1934, no México. ",
-                afirmacao: [
-                " Criada no dia 13 de julho. ",
-                "2",
-                "3"
-                ]
-            }
-        ]
-    },
-    {
-        enunciado: " Qual era o nome da taça antiga?",
-        alternativas: [
-            {
-                texto: " Até 1970, o troféu era conhecido como Taça Jules Rimet.  ",
-                afirmacao: 
-                " Criador da Copa Jules Rimet. ",
-            },
-            {
-                texto: " Taça das Nações Unidas ",
-                afirmacao: " Liga das Nações da UEFA ",
-            }
-        ]
-    },
-    {
-        enunciado: " Qual país foi o anfitrião da Copa do Mundo de 2014? ",
-        alternativas: [
-            {
-                texto: " Brasil ",
-                afirmacao: " O Brasil sediou a Copa do Mundo de 2014. ",
-            },
-            {
-                texto: " Argentina ",
-                afirmacao: " A Argentina sediou a Copa do Mundo de 2014. ",
-            }
-        ]
-    },
-    {
-        enunciado: " Qual país sediou a Copa do Mundo de 2022? ",
-        alternativas: [
-            {
-                texto: " Catar   ",
-                afirmacao: " O Catar sediou a Copa do Mundo de 2022. ",
-            },
-            {
-                texto: " Russia   ",
-                afirmacao: " O Russia sediou a Copa do Mundo de 2022. ",
-            }
-        ]
-    },
-    {
-        enunciado: " Quantos títulos o Brasil tem na Copa do Mundo?   ",
-        alternativas: [
-            {
-                texto: " 5 Titulos ",
-                afirmacao: "O Brasil é o país com mais títulos, tendo vencido a Copa do Mundo 5 vezes. ",
-            },
-            {
-                texto: " 4 Titulos ",
-                afirmacao: " O Brasil é o país com mais títulos, tendo vencido a Copa do Mundo 4 vezes. ",
-            }
-        ]
-    },
-];
 
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = " ";
 
-function mostraPergunta(){
+function mostraPergunta() {
 
-    if (atual >= perguntas.length){
-        mostraResultado();
-        return;
-    }
-
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = " ";
-    mostraAlternativas();
+if (atual >= perguntas.length) {
+mostraResultado();
+return;
 }
 
-function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    } 
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+caixaAlternativas.textContent = " ";
+mostraAlternativas();
 }
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
+function mostraAlternativas() {
+for (const alternativa of perguntaAtual.alternativas) {
+const botaoAlternativas = document.createElement("button");
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+caixaAlternativas.appendChild(botaoAlternativas);
+}
 }
 
-function mostraResultado(){
-    caixaPerguntas.textContent = "Se fosse possível traduzir sua forma de aprender em palavras, diríamos que...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = " ";
+function respostaSelecionada(opcaoSelecionada) {
+const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+historiaFinal += afirmacoes + " ";
+atual++;
+mostraPergunta();
+}
+
+function mostraResultado() {
+caixaPerguntas.textContent = "Se fosse possível traduzir sua forma de aprender em palavras, diríamos que...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = " ";
+caixaResultado.classList.add("mostrar");
+botaoJogarNovamente.addEventListener("click", jogaNovamente);
+}
+
+function jogaNovamente(){
+atual = 0;
+historiaFinal = " ";
+caixaResultado.classList.remove("mostrar");
+mostraPergunta();
 }
 
 mostraPergunta();
---
+++ pergunta.js
@@ -0,0 +1,77 @@
+ export const perguntas = [
+    {
+        enunciado: "Quando foi desenvolvido a primeira copa do mundo?",
+        alternativas: [
+            {
+                texto: " 13 de julho de 1930, no Uruguai.",
+                afirmacao: [
+                " Criada em 1930 no Uruguai. ",
+                "afirmacao 2",
+                "afirmacao 3"
+
+                ]
+            },
+            {
+                texto: " Criada  13 de julho de 1934, no México. ",
+                afirmacao: [
+                " Criada no dia 13 de julho. ",
+                "2",
+                "3"
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: " Qual era o nome da taça antiga?",
+        alternativas: [
+            {
+                texto: " Até 1970, o troféu era conhecido como Taça Jules Rimet.  ",
+                afirmacao: 
+                " Criador da Copa Jules Rimet. ",
+            },
+            {
+                texto: " Taça das Nações Unidas ",
+                afirmacao: " Liga das Nações da UEFA ",
+            }
+        ]
+    },
+    {
+        enunciado: " Qual país foi o anfitrião da Copa do Mundo de 2014? ",
+        alternativas: [
+            {
+                texto: " Brasil ",
+                afirmacao: " O Brasil sediou a Copa do Mundo de 2014. ",
+            },
+            {
+                texto: " Argentina ",
+                afirmacao: " A Argentina sediou a Copa do Mundo de 2014. ",
+            }
+        ]
+    },
+    {
+        enunciado: " Qual país sediou a Copa do Mundo de 2022? ",
+        alternativas: [
+            {
+                texto: " Catar   ",
+                afirmacao: " O Catar sediou a Copa do Mundo de 2022. ",
+            },
+            {
+                texto: " Russia   ",
+                afirmacao: " O Russia sediou a Copa do Mundo de 2022. ",
+            }
+        ]
+    },
+    {
+        enunciado: " Quantos títulos o Brasil tem na Copa do Mundo?   ",
+        alternativas: [
+            {
+                texto: " 5 Titulos ",
+                afirmacao: "O Brasil é o país com mais títulos, tendo vencido a Copa do Mundo 5 vezes. ",
+            },
+            {
+                texto: " 4 Titulos ",
+                afirmacao: " O Brasil é o país com mais títulos, tendo vencido a Copa do Mundo 4 vezes. ",
+            }
+        ]
+    },
+];
